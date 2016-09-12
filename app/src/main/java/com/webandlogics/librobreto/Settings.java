2a51a5c50b963ae78c739c824c6bbe1b97bd41db
Alvaro Marquina <thesunno@gmail.com>
2016-09-12 18:48:47 -0400

    Settings

--- app/src/main/java/com/webandlogics/librobreto/Settings.java
+++ app/src/main/java/com/webandlogics/librobreto/Settings.java
@@ -2,8 +2,16 @@ package com.webandlogics.librobreto;
 
 /**
  * Created by alvaro on 11/09/16.
- * URL's and that kind of stuff
+ * URL's, keys and that kind of stuff
  */
 public class Settings {
     public static final String API_BASE_URL = "https://www.goodreads.com";
+    public static final String API_KEY = BuildConfig.API_KEY;
+    public static final String API_SECRET = BuildConfig.API_SECRET;
+
+    // This MUST be the same as in data param in login activity in manifest file
+    public static final String OAUTH_REDIRECT_URI = "librobreto://librobreto/login";
+
+    // Logging
+    final static public String TAG = "Librobreto";
 }
--- app/src/main/java/com/webandlogics/librobreto/http/GoodReadsAPI.java
+++ app/src/main/java/com/webandlogics/librobreto/http/GoodReadsAPI.java
@@ -1,6 +1,7 @@
 package com.webandlogics.librobreto.http;
 
 import com.webandlogics.librobreto.BuildConfig;
+import com.webandlogics.librobreto.Settings;
 import com.webandlogics.librobreto.http.entities.search.SearchResponse;
 
 import retrofit2.Call;
@@ -13,7 +14,7 @@ import retrofit2.http.Query;
  */
 public interface GoodReadsAPI {
 
-    final static String Q3M_P6_LBIRFX_CFB_XDL4W = BuildConfig.API_KEY;
+    final static String Q3M_P6_LBIRFX_CFB_XDL4W = Settings.API_KEY;
 
     @GET("search/index.xml?key=" + Q3M_P6_LBIRFX_CFB_XDL4W)
     Call<SearchResponse> searchBook(@Query("q") String q);
